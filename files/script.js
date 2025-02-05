708df1477b811ae424383ec81f7d7f48557eb3d6
Akshaya E <akshayaeroth@gmail.com>
2025-02-05 22:36:36 +0530

    Update script.js

--- files/script.js
+++ files/script.js
@@ -33,7 +33,7 @@ window.onscroll = () => {
 
 /*========== Typing animation in home page ==========*/
 var typed = new Typed(".text", {
-    strings: ["Data Engineering","Networking" , "Data Analysis", "Data Visualization", "Data Management","Programming"],
+    strings: ["Data Engineering", "Networking", "Data Analysis", "Data Visualization", "Data Management"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
